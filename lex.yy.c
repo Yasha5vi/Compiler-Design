dbe78ef0155df086a627ad0b39b1a3d5d2af63ec
Yashasvi Chandra <146069599+Yasha5vi@users.noreply.github.com>
2025-02-27 10:11:40 +0000

    27 feb work yet to do

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,10 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[13] =
+static const flex_int16_t yy_accept[7] =
     {   0,
-        0,    0,    5,    3,    3,    0,    1,    0,    0,    1,
-        2,    0
+        0,    0,    3,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -372,7 +371,7 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,33 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[3] =
     {   0,
-        1,    2,    1,    1
+        1,    1
     } ;
 
-static const flex_int16_t yy_base[15] =
+static const flex_int16_t yy_base[7] =
     {   0,
-        0,    0,   13,   14,    2,    9,    0,    8,    6,    0,
-       14,   14,    6,    8
+        0,    0,    3,    4,    4,    4
     } ;
 
-static const flex_int16_t yy_def[15] =
+static const flex_int16_t yy_def[7] =
     {   0,
-       12,    1,   12,   12,   12,   13,   14,   13,    8,   14,
-       12,    0,   12,   12
+        6,    1,    6,    6,    6,    0
     } ;
 
-static const flex_int16_t yy_nxt[19] =
+static const flex_int16_t yy_nxt[7] =
     {   0,
-        4,    4,    4,    5,    6,    7,    8,    8,   10,   11,
-        9,    9,   12,    3,   12,   12,   12,   12
+        4,    5,    6,    3,    6,    6
     } ;
 
-static const flex_int16_t yy_chk[19] =
+static const flex_int16_t yy_chk[7] =
     {   0,
-        1,    1,    1,    1,    5,    5,   13,   13,   14,    9,
-        8,    6,    3,   12,   12,   12,   12,   12
+        1,    1,    3,    6,    6,    6
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,13 +436,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "19feb_skipComments2.l"
-#line 2 "19feb_skipComments2.l"
-    #include <stdio.h>
-    #include <stdlib.h>
-    int count = 0; // To count comments
-#line 450 "lex.yy.c"
-#line 451 "lex.yy.c"
+#line 1 "27feb_tokenizing.l"
+#line 2 "27feb_tokenizing.l"
+    #include<stdio.h>
+#line 443 "lex.yy.c"
+#line 444 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -664,10 +657,9 @@ YY_DECL
 		}
 
 	{
-#line 7 "19feb_skipComments2.l"
+#line 4 "27feb_tokenizing.l"
 
-
-#line 671 "lex.yy.c"
+#line 663 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -694,13 +686,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 13 )
+				if ( yy_current_state >= 7 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 14 );
+		while ( yy_base[yy_current_state] != 4 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -726,28 +718,15 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 9 "19feb_skipComments2.l"
-{ count++; }
+#line 5 "27feb_tokenizing.l"
+{printf("%c",yytext[0]);}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 11 "19feb_skipComments2.l"
-{ count++; }
-	YY_BREAK
-case 3:
-/* rule 3 can match eol */
-YY_RULE_SETUP
-#line 14 "19feb_skipComments2.l"
-{ fprintf(yyout, "%s", yytext);
-	  printf ("%s",yytext); }
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 17 "19feb_skipComments2.l"
+#line 7 "27feb_tokenizing.l"
 ECHO;
 	YY_BREAK
-#line 751 "lex.yy.c"
+#line 730 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1044,7 +1023,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 13 )
+			if ( yy_current_state >= 7 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1072,11 +1051,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 13 )
+		if ( yy_current_state >= 7 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 12);
+	yy_is_jam = (yy_current_state == 6);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1752,42 +1731,22 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 17 "19feb_skipComments2.l"
+#line 7 "27feb_tokenizing.l"
 
-
-int yywrap() {
+/*
+design a lex code for tokenizing operator, delimitor, separator, identifiers ,keywords of the following C fragment
+*/
+int yywrap(){
     return 1;
 }
-
-int main() {
-    FILE *f1, *f2;
-
-    // Open input file
-    f1 = fopen("comment.c", "r");
-    if (!f1) {
-        printf("Error: Cannot open input file.\n");
-        return 1;
+int main(){
+    FILE *ptr = fopen("in.txt","r");
+    if(!ptr){
+        printf("Error opening File\n");
+        return 0;
     }
-    yyin = f1;  // Set Lex input file
-
-    // Open output file
-    f2 = fopen("comment1.c", "w");
-    if (!f2) {
-        printf("Error: Cannot open output file.\n");
-        fclose(f1);
-        return 1;
-    }
-    yyout = f2;  // Set Lex output file
-
-    // Call Lex Scanner
+    yyin = ptr;
     yylex();
-
-    // Print comment count
-    printf("The number of comments removed: %d\n", count);
-
-    // Close files
-    fclose(f1);
-    fclose(f2);
-
+    fclose(ptr);
     return 0;
 }
